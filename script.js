0bc84581f0a77b63ecd6daf79b9a8198f017d7b4
Anderson <andersonfabri31@gmail.com>
2022-08-05 18:26:49 -0300

    Entrega pronta

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Anderson"
+let sobrenome = "Rodrigues"
+let cidade = "Iretama"
+let anoNascimento = 2001;
+let anoAtual = 2022;
+let idade = anoAtual - anoNascimento ;
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
